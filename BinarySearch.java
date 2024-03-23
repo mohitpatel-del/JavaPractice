d750064403ce7037fac134497d7e9db1ad6c47f0
Mohit Patel <58075596+mohitpatel-del@users.noreply.github.com>
2024-03-23 21:07:18 +0530

    BinarySearch.java
    A Simple Binary Search algorithm.
    
    Input:
    1. Size of Array
    2. Elements in sorted order.
    3. Target element (Elements searching for)
    
    Output:
    Index of the target element.

---
+++ BinarySearch.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("Enter Size of the Array");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter The elements of the Array");
+        for(int i =0; i<size;i++){
+            System.out.println("Enter Elment no. "+ i);
+            arr[i] = sc.nextInt();
+            if(i>=1){
+                if(arr[i]<arr[i-1]){
+                    System.out.println("Invalid input: Array Should be in sorted order.");
+                    break;
+                }
+            }
+        }
+        System.out.println("Enter the Target number to find: ");
+        int target = sc.nextInt();
+        int res = binaryS(arr,target);
+        System.out.println("Index = "+ res+"   element = "+ arr[res]);
+
+
+    }
+    static int binaryS(int[] arr, int target){
+        int res = 0;
+        int s = 0;
+        int e = arr.length;
+
+        for(int i = s; i<e;i++){
+            int mid = (s+e)/2;
+
+            if(arr[mid]==target){
+                res = mid;
+                break;
+            } else if(arr[mid]>target) {
+                e=mid;
+            } else if (arr[mid]<target) {
+                s=mid;
+            }
+        }
+        return res;
+    }
+}
